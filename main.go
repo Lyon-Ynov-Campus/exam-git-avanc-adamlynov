ac72fd4b488a7ae0bf2fb0e328d6433b6cdf8c53
adamlynov <pavois-dent-1b@icloud.com>
2025-10-27 11:04:09 +0100

    Ajout de la page /color avec fond vert

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
-	})
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
 
-	fmt.Println("Serveur démarré sur http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func main() {
+	http.HandleFunc("/color", ColorHandler)
+	http.ListenAndServe(":8080", nil)
 }
